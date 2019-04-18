4bef9b48ba718830e23d3df30e519962f63f759e
Tina Huo <th2221@nyu.edu>
2019-04-18 02:05:26 -0400

    first commit

---
+++ 4.18_hw/script.js
@@ -0,0 +1,25 @@
+function square(){
+    var square=document.getElementById('square-input').value
+	var result=number**2
+    alert("The result of squaring the number "+square+" is "+result+".")
+}
+
+function halfNumber(){
+    var half=document.getElementById('half-input').value 
+    alert("Half of "+ half+" is "+half2/2+".")
+}
+
+function percentOf(){
+    var number1=document.getElementById('percent1-input').value 
+    var number2=document.getElementById('percent2-input').value
+	var percent= number1/number2*100
+	var rounded = percent.toFixed(2);
+    alert(number1+" is "+rounded+"% of "+number2)
+}
+
+function areaOfCirle(){
+    var radius=document.getElementById('area-input').value
+	var area=radius**2*3.1415926
+	var rounded2 = area.toFixed(2);
+    alert("The area for a circle with "+radius+" is "+ rounded2)
+}
